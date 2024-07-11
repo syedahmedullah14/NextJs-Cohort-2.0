9ad2ce8f809041b21e3261f19737fb4a74651822
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-11 22:22:55 +0530

    Sign in route

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,7 @@
+import SigninComponent from "@/app/components/SigninComponent";
+
+export default function Signin() {
+    return <div className="h-screen flex justify-center flex-col">
+        <SigninComponent />
+    </div>
+}
