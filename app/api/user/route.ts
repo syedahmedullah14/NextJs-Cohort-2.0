3010c425498583d1b9e8f9ecfb4ec456b169341a
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-12 14:01:42 +0530

    POST endpoint

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,8 +1,13 @@
-import { NextResponse } from "next/server";
+// ap/api/user/route.ts
+import { NextResponse } from 'next/server';
 
-export async function GET(){
-    return NextResponse.json({
-        email: 'jaser', 
-        name: 'jaser'
-    })
+export async function POST(request: Request) {
+  const { username, password } = await request.json();
+
+  // Here, you would typically perform validation and store the user data in a database
+  // For simplicity, we'll just log the username and password to the console
+  console.log('Username:', username);
+  console.log('Password:', password);
+
+  return NextResponse.json({ message: 'Sign up successful' });
 }
