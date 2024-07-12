20addd3b9979dda9d98532dfc5a4f9ea35e8be6b
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-12 09:59:59 +0530

    Prettify async component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
-import axios from "axios";
-import { use } from "react";
+import axios from 'axios';
 
 async function getUserDetails() {
   const response = await axios.get('https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details');
   return response.data;
 }
 
-export default async function Home(){
+export default async function Home() {
   const userData = await getUserDetails();
-  
+
   return (
-    <div>
-      {userData.email}
-      {userData.name}
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>
+            Name: {userData?.name}
+          </div>
+          {userData?.email}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
