efbea6d52a0be8e38be8c5dcb4d023520bda8c25
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-12 09:58:27 +0530

    Added an async component as NextJs supports one

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
 import axios from "axios";
+import { use } from "react";
 
 async function getUserDetails() {
   const response = await axios.get('https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details');
   return response.data;
+}
+
+export default async function Home(){
+  const userData = await getUserDetails();
+  
+  return (
+    <div>
+      {userData.email}
+      {userData.name}
+    </div>
+  )
 }
