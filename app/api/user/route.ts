31dd0050ac039aec5c6c17e51c3177b9f8364a2a
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-12 17:11:16 +0530

    Added GET endpoint

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from "@prisma/client";
 
+const client = new PrismaClient();
+
+
 export async function POST(req: NextRequest) {
   const body = await req.json();
   // Add validation logic here (e.g., using Zod)
@@ -15,4 +18,10 @@ export async function POST(req: NextRequest) {
   console.log(user.id);
 
   return NextResponse.json({ message: 'Signed up' });
+}
+
+
+export async function GET() {
+  const user = await client.user.findFirst({});
+  return NextResponse.json({ name: user?.username, email: user?.username });
 }
