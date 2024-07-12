a16503bc62c89bea6e9eebb48d95b9be9215ca12
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-12 17:06:25 +0530

    Using database logic in API route

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from "@prisma/client";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
+  // Add validation logic here (e.g., using Zod)
 
-  return NextResponse.json({ username: body.username, password: body.password });
+  const user = await client.user.create({
+    data: {
+      username: body.username,
+      password: body.password,
+    },
+  });
+
+  console.log(user.id);
+
+  return NextResponse.json({ message: 'Signed up' });
 }
