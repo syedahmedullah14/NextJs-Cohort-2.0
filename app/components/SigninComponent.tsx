10a3163964d708b0ca94787e2ff7b10a379ba601
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-13 12:16:59 +0530

    Default exported SigninComponent

--- app/components/SigninComponent.tsx
+++ app/components/SigninComponent.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { ChangeEventHandler, useState } from "react";
 import { useRouter } from "next/navigation";
 
-export function Signup() {
+export default function Signup() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const router = useRouter()
